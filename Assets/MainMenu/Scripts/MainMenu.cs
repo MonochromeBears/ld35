baae18a38fb9ba9bfc4523260beddcd19d157aa6
Maxim <raincoven@gmail.com>
2016-04-16 18:15:51 +0600

    Added: main menu scene

---
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture backgroundTextere;
+
+	void OnGUI () {
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTextere);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
