5c0fcc91cf96f6d1fbedc11fc27c5a8bff940fd9
Maxim <raincoven@gmail.com>
2016-04-16 22:24:40 +0600

    Added: npv random valking

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,9 +7,12 @@ public class EnemyController : UnitController {
 	public bool follow = false;
 	public bool flirtFails = false;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle * (0.5f + 0.5f * Random.value) * 50f;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		transform.localRotation = Quaternion.Euler(Random.value * 360, Random.value * 360, Random.value * 360);
 	}
 	
 	// Update is called once per frame
