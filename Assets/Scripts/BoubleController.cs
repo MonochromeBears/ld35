ba1c3063e7927f024c3a47ed16d22f78c068d5b6
Maxim <raincoven@gmail.com>
2016-04-17 17:45:17 +0600

    Fixed: bouble

---
+++ Assets/Scripts/BoubleController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoubleController : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public Transform target;
+	public float showTime = 5;
+	public float waitShowTime = 25;
+
+	private float showTimeDump = 0;
+	private float waitShowTimeDump = 0;
+	private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+		this.sprite = this.transform.GetComponent<SpriteRenderer>();
+		this.sprite.color = new Color(1f,1f,1f,0f);
+		int index = target.GetComponent<EnemyController>().flirtType;
+		this.sprite.sprite = this.sprites[index-1];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.showTimeDump -= Time.deltaTime;
+		this.waitShowTimeDump -= Time.deltaTime;
+		if (this.showTimeDump < 0) {
+			this.sprite.color = new Color(1f,1f,1f,0f);
+		}
+		if (((int) (Random.value * 10) == 1) && this.waitShowTimeDump < 0) {
+			this.sprite.color = new Color(1f,1f,1f,1f);
+			this.showTimeDump = this.showTime;
+			this.waitShowTimeDump = this.waitShowTime;
+		}
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float levelTime = 300f;
+
+	private float countdown;
+	private float timeBarPosYStart;
+	private float timeBarHeight = 348;
+
+	// Use this for initialization
+	void Start () {
+		countdown = levelTime;
+		timeBarPosYStart = this.transform.position.y;
+		timeBarHeight = this.transform.GetComponent<SpriteRenderer>().bounds.size.y * this.transform.localScale.y;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (countdown <= 0) {
+			// Game Over
+		}
+		countdown -= Time.deltaTime;
+
+		this.updateBars();
+	}
+
+	void updateBars()
+	{
+		float bareScale = (levelTime - countdown) / levelTime;
+		float posTimeY = timeBarPosYStart + bareScale * timeBarHeight / 2 - timeBarHeight/2;
+		this.transform.position = new Vector3(this.transform.position.x, posTimeY, this.transform.position.z);
+		this.transform.localScale = new Vector3(this.transform.localScale.x, bareScale, this.transform.localScale.z);
+	}
+}
